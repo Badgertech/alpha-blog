470ec24dfd78660f16fafddf366af9326caad9a3
badgertech <robert.badger@justinbrands.com>
2016-05-06 22:03:41 +0000

    add users via UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
   # See how all your routes lay out with "rake routes".
   root 'pages#index'
   get 'pages/about', to: 'pages#about'
