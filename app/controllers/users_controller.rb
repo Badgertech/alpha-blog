cdfbf1a65e2adef26837faa8c95a5800ae9a029c
badgertech <robert.badger@justinbrands.com>
2016-05-12 13:10:55 +0000

    added edit users features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,24 @@ class UsersController <ApplicationController
     if @user.save
       flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
       redirect_to articles_path
-      
-      else
+    else
       render 'new'
-      
     end
   end
-  private
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Your account was updated successfully"
+      redirect_to articles_path
+      else
+      render 'edit'
+  end
+  end
+ private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-end
+  end
