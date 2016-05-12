c2fcb47f83da1dcfd780520e38c87a4cfa99e85c
badgertech <robert.badger@justinbrands.com>
2016-05-12 21:56:56 +0000

    Add logon and logoff

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,13 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   get 'signup', to: 'users#new'
   resources :users, except: [:new]
+
+  get 'login', to: "sessions#new"
   # See how all your routes lay out with "rake routes".
   root 'pages#index'
   get 'pages/about', to: 'pages#about'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
