2a77184a66f8c60ec4b4b78d76fe94631a6a537f
badgertech <robert.badger@justinbrands.com>
2016-07-09 12:24:27 +0000

    Add categories model and test

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get 'pages/about', to: 'pages#about'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  resources :categories, except: [:destroy]
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
